60cd4838a494a49f13c17dab484e012419fb86c9
ssjcaba <ssjcabaa@gmail.com>
2026-03-03 14:55:54 -0500

    Implemented a function that finds any collision risks.

--- src/risk.py
+++ src/risk.py
@@ -1,4 +1,4 @@
-#Step 1: Convert sastellite positions into geocentric coordinates
+
 import math
 
 def calc_distance(row1, row2):
@@ -7,4 +7,25 @@ def calc_distance(row1, row2):
         dz = row1['z'] - row2['z']
 
         dist = math.sqrt(dx**2 + dy**2 + dz**2)
+
+#Below is a function that takes my entire table 
+# And compares every satellite to each other to find
+#the danger zone pairs
+
+def find_risks(df):
+    risks = []
+    for i in range(len(df)):
+
+        for j in range(i + 1, len(df)):
+            sat1 = df.iloc[i]
+            sat2 = df.iloc[j]
+
+            dist = calc_distance(sat1,sat2)
+            if dist < 700:
+                risks.append({
+                    "satellite_1": sat1['name'],
+                    "satellite_2": sat2['name'],
+                    "distance": dist
+                })
+    return risks 
 
