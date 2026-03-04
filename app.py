d4479181cad23221875b4334010ee4d9d0081e29
ssjcaba <ssjcabaa@gmail.com>
2026-03-04 00:52:57 -0500

    Implemented collision monitorung UI

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import folium
 import pandas as pd
 from skyfield.api import load
 from streamlit_folium import st_folium
+from src.risk import find_risks
 
 # Import your custom functions
 from src.parse_tle import load_tle_records, compute_positions
@@ -24,7 +25,26 @@ def get_satellite_data():
 
 # 3. EXECUTION: Get the data and build the map
 df = get_satellite_data()
+danger = find_risks(df)
 my_map = create_map(df) 
 
 # 4. DISPLAY: Added a 'key' here to tell Streamlit not to refresh the component
 st_folium(my_map, width=700, height=500, key="orbital_map")
+
+
+
+st.title("Risk of Collision")
+
+with st.sidebar:
+    st.header("🛰️ Collision Alerts")
+    
+    if len(danger) > 0:
+        st.warning(f"Found {len(danger)} risks")
+        
+        for risk in danger:
+            st.write(f"**{risk['satellite_1']}** vs **{risk['satellite_2']}**")
+            st.write(f"Distance: {risk['distance']} km")
+            st.divider() 
+            
+    else:
+        st.success("All clear! No risks found.")
